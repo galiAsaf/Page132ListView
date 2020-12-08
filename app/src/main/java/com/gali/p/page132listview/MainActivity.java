774131cca7b0a2ad0c1e5c2471433a1bac2b03dc
ggaallii11 <gali8asaf@gmail.com>
2020-12-08 15:48:30 +0200

    I'M NOT FINISH YET...

--- app/src/main/java/com/gali/p/page132listview/MainActivity.java
+++ app/src/main/java/com/gali/p/page132listview/MainActivity.java
@@ -1,13 +1,20 @@
 package com.gali.p.page132listview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +26,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSongData();
-
         latView1= findViewById(R.id.list1);
 
-        latView1.setAdapter(SongAdapter);
+        getSongData();
+
+        latView1 = (ListView) findViewById(R.id.list1);
+
+        SongAdapter sa= new SongAdapter(this, R.layout.song, arySong);
+        latView1.setAdapter(sa);
     }
 
     public void getSongData(){
         Song s;
         for (int i =1; i<= 10; i++)
         {
-            s=new Song("img1.png", "song name"+i, "singer name"+i, 6);
+            s=new Song("img1.jpg", "song name"+i, "singer name"+i, i);
             arySong.add(s);
         }
     }
@@ -39,7 +49,37 @@ public class MainActivity extends AppCompatActivity {
 
     public class SongAdapter extends ArrayAdapter<Song>
     {
+        private Context ctx;
+        private int songResourceId;
+        private List<Song> data;
 
+        public SongAdapter(Context context, int resource, List<Song> data) {
+            super(context, resource);
+            this.ctx = (MainActivity) context;
+            this.songResourceId = resource;
+            this.data = data;
+        }
+
+        public int getCount(){
+            return data.size();
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            LayoutInflater li = (LayoutInflater) this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View v = li.inflate(this.songResourceId,null);
+
+            Song s = this.data.get(position);
+            TextView tv1= v.findViewById(R.id.songName);
+            tv1.setText(s.getSongName());
+            TextView tv2= v.findViewById(R.id.singerName);
+            tv2.setText(s.getSingerName());
+            TextView tv3= v.findViewById(R.id.time);
+            tv3.setText(String.valueOf(s.getTime()));
+
+            return v;
+        }
     }
 
 }
